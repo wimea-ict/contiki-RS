b6002062540f79a2c17c8a96f134aceabb2ff997
bjorn@sa0bxi <bjorn@pehrson.se>
2015-09-25 22:09:13 +0200

    Changes to be committed: modified:   flash.sh Added initiation of bootloader (upgr) via tty_talk

--- platform/avr-RSS2/apps/rime-bc/flash.sh
+++ platform/avr-RSS2/apps/rime-bc/flash.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
 make bc hex TARGET=avr-RSS2
-
-avrdude -p m128rfa1 -c avr109 -P /dev/ttyUSB0 -b 38400 -e -U flash:w:bc.hex
+/usr/bin/stty sane
+/usr/sbin/tty_talk -38400 /dev/ttyUSB0 "upgr
+"
+/usr/bin/avrdude -p m128rfa1 -c avr109 -P /dev/ttyUSB0 -b 38400 -e -U flash:w:bc.hex
