187e2a32845fc63eb1cbe0270d6f57326eaec5df
Robert Olsson <robert@herjulf.se>
2015-10-09 08:29:22 +0200

    Removed absoluet to commands. Added support for stk500v2 bootloader 	modified:   flash.sh

--- platform/avr-RSS2/apps/rime-bc/flash.sh
+++ platform/avr-RSS2/apps/rime-bc/flash.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 make bc hex TARGET=avr-RSS2
-/usr/bin/stty sane
-/usr/sbin/tty_talk -38400 /dev/ttyUSB0 "upgr
+
+S_PROG=avr109
+#S_PROG=stk500v2
+
+PGM=
+
+stty sane
+tty_talk -38400 /dev/ttyUSB0 "upgr
 "
-/usr/bin/avrdude -p m128rfa1 -c avr109 -P /dev/ttyUSB0 -b 38400 -e -U flash:w:bc.hex
+avrdude -p m128rfa1 -c $S_PROG -P /dev/ttyUSB0 -b 38400 -e -U flash:w:bc.hex
